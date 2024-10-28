f28b79943f507ce226cf5168467c8d1c965b846d
Lucas <lucasdamata01@hotmail.com>
2024-10-28 14:13:20 -0300

    Paredes de tamanhos variados

---
+++ ComputacaoGrafica/Assets/Script/scr_Gerador.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Gerador : MonoBehaviour
+{
+    public GameObject parede;
+
+    public float max_x;
+    public float min_x;
+
+    public float spawn_time = 60.0f; // Tempo em segundos entre os spawns
+
+    private GameObject curr_parede;
+    private float timer;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = spawn_time;
+
+        GerarNovaParede();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            GerarNovaParede();
+            timer = spawn_time;
+        }
+    }
+
+    void GerarNovaParede()
+    {
+        // Pega o tamanho da parede
+        float scale = parede.transform.localScale.x;
+
+        // Acha uma nova posição em x para a parede
+        float x_pos = Random.Range(max_x-scale/2, min_x+scale/2);
+
+        // Cria uma nova parede
+        curr_parede = Instantiate(parede);
+        curr_parede.transform.position = new Vector3(x_pos, 0.5f, 0.0f) + transform.position;
+    }
+}
--- ComputacaoGrafica/Assets/Script/scr_Parede.cs
+++ ComputacaoGrafica/Assets/Script/scr_Parede.cs
@@ -12,7 +12,10 @@ public class scr_Parede : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float x_scale = Random.Range(5, 10);
+        float y_scale = Random.Range(1, 3);
+        transform.localScale = new Vector3(x_scale, y_scale, 0.5f);
+        transform.position = new Vector3(transform.position.x, y_scale/2, transform.position.z);
     }
 
     // Update is called once per frame
